b8464530b1b6b3d65bbd47541d7e8c15b4e4f7dc
huang840801 <david840801@gmail.com>
2018-05-22 22:30:19 +0800

    Spinner complete

--- app/src/main/java/com/huang/hello/exercise_record/MainActivity.java
+++ app/src/main/java/com/huang/hello/exercise_record/MainActivity.java
@@ -1,12 +1,14 @@
 package com.huang.hello.exercise_record;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.content.SharedPreferences;
 import android.widget.Toast;
 
 
@@ -21,10 +23,33 @@ public class MainActivity extends AppCompatActivity {
     public static final String TEXT="text";
 
     private String text;
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence>adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner=findViewById(R.id.spinner);
+        adapter=ArrayAdapter.createFromResource(this,R.array.Sports_name,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+       spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+               Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+"已選擇",Toast.LENGTH_LONG).show();
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> parent) {
+
+           }
+       });
+
+
 
         usernameinput=findViewById(R.id.et_username);
         textViewShow=(TextView)findViewById(R.id.tv_show);
